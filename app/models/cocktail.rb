4e4dd53dc2b914495dee685dec2fc903bb3a68d9
Marina Jeanbart <marinajeanbart@gmail.com>
2020-11-13 15:26:54 +0100

    done

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,6 @@ class Cocktail < ApplicationRecord
   has_many :doses, :dependent => :delete_all
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+  has_one_attached :photo
+
 end
